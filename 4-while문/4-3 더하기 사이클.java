d70105bdcb5c10ee7189ceb21f919c8393cd7fcc
solasolabeam <64000823+solasolabeam@users.noreply.github.com>
2022-01-30 01:01:06 +0900

    알고리즘 연습

---
+++ 4-while문/4-3 더하기 사이클.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		in.close();
+		int cnt=0;
+		int e = N;
+		
+		while(true) {
+			N=((N%10)*10)+((( N/10)+(N%10))%10);
+			cnt++;
+			if(e==N) {
+				
+				break;
+			}
+		}
+		
+		System.out.println(cnt);
+		
+
+			
+		
+
+}
+}
