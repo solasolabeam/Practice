ccaa3674669dda4d07c99f9aa7eb6390f877ad79
solasolabeam <64000823+solasolabeam@users.noreply.github.com>
2022-02-01 20:13:14 +0900

    알고리즘 연습

---
+++ Bronze/18108.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+                        in.close();
+
+                        int sum=a-543;
+                       
+                        System.out.println(sum);
+
+				
+}
+}
