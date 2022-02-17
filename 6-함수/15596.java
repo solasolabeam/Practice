28014dd694fd361948542cb9a410ac84a90ee551
Sol Na <nathf1005@naver.com>
2022-02-17 15:46:03 +0900

    알고리즘 연습

---
+++ 6-함수/15596.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+	
+		
+		long sum =A+B;
+		
+		System.out.println(sum);
+		in.close();
+	}
+}
