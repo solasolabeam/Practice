ed1921daaa7a2e72d36cebdf3155ca1e14499579
Sol Na <nathf1005@naver.com>
2022-02-25 23:06:57 +0900

    알고리즘 연습

---
+++ Bronze/16486.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test2 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int d1 =in.nextInt();
+		int d2 = in.nextInt();
+		
+		System.out.println((d1*2)+(d2*2*3.141592));
+		
+		
+	}
+}
