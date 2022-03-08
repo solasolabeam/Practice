a2c62f218cf8fb47c93def64738f6acb1ac3490a
Sol Na <nathf1005@naver.com>
2022-03-09 01:42:20 +0900

    math.BigInteger

---
+++ Bronze/13277.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		BigInteger A = in.nextBigInteger();
+		BigInteger B = in.nextBigInteger();
+		System.out.println(A.multiply(B));
+		/*
+		 * add() 더하기
+		 * subtract() 빼기
+		 * multiply() 곱하기
+		 * divide() 나누기
+		 * remainder() 나머지
+		 */
+
+	}
+}
