8c49cc0ae6f9ce584b6fcf48bedae738d842b33a
Sol Na <nathf1005@naver.com>
2022-03-09 22:26:45 +0900

    #1.BufferedReader/split

--- Bronze/20499.java
+++ Bronze/20499.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int K =in.nextInt();
-		int D = in.nextInt();
-		int A = in.nextInt();
-		if(K+A<D||D==0) {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		//split은 지정해준 문자를 기준으로 배열에 나눠서 저장하는 것이다.
+		//K/D/A가 입력되고 /기준으로 배열에 나눠 저장하는 것이기 떄문에 배열에는
+		//string 배열에 {K, D, A}가 들어간다.
+		String[] arr = S.split("/");
+		int K = Integer.parseInt(arr[0]);
+		int D = Integer.parseInt(arr[1]);
+		int A = Integer.parseInt(arr[2]);
+		
+		if((K + A) < D || D == 0) {
 			System.out.println("hasu");
-		}
-		else {
+		}else {
 			System.out.println("gosu");
 		}
-
 	}
+
 }
