c9c2ee8ae6d4a0025cb50446ec3191b784a2f1c4
solasolabeam <nathf1005@naver.com>
2022-03-14 23:55:15 +0900

    알고리즘 연습

---
+++ Bronze/11382.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] t) throws IOException{
+	Scanner in =new Scanner(System.in);
+	long A = in.nextLong();
+	long B = in.nextLong();
+	long C = in.nextLong();
+	System.out.println(A+B+C);
+
+	
+	}
+}
